6fe3e5e654eea6738a46e086494b3b4d7a5aa5f1
NakulPandey <141328991+NakulPandey5599@users.noreply.github.com>
2026-01-18 12:35:07 +0530

    merging array

--- day16.php
+++ day16.php
@@ -9,3 +9,22 @@ foreach ($arr as $value) {
     }
 }
 echo $max;
+
+
+
+
+
+
+/////
+
+
+$arr1 = [1, 2, 3];
+$arr2 = [4, 5, 6];
+
+$result = $arr1;
+
+foreach ($arr2 as $value) {
+    $result[] = $value;
+}
+
+print_r($result);
