192d3efbcb4c37112ff6c8c6ca33395855a1b2b4
NakulPandey <141328991+NakulPandey5599@users.noreply.github.com>
2026-01-21 00:14:01 +0530

    updated

--- non-repetative.php
+++ non-repetative.php
@@ -11,3 +11,13 @@ for ($i = 0; $i < strlen($str); $i++) {
 }
 
 
+
+$str = "swiss";
+for ($i = 0; $i < strlen($str); $i++) {
+    if (substr_count($str, $str[$i]) == 1) {
+        echo $str[$i];
+        break;
+    }
+}
+
+
