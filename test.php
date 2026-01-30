09b07cf4e542514ee5518f8e98fa6257fcd40b82
NakulPandey <141328991+NakulPandey5599@users.noreply.github.com>
2026-01-30 23:44:01 +0530

    reversing the string

--- test.php
+++ test.php
@@ -10,3 +10,15 @@ foreach ($arr as $value) {
 }
 
 echo $max;
+
+
+<?php
+$str = "interview";
+$rev = "";
+
+for ($i = strlen($str) - 1; $i >= 0; $i--) {
+    $rev .= $str[$i];
+}
+
+echo $rev; // weivretni
+?>
