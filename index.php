0f11f8cfbf4f9e5a42703f4cf639629e1fe66626
NakulPandey <141328991+NakulPandey5599@users.noreply.github.com>
2026-02-02 23:54:44 +0530

    associative array

--- index.php
+++ index.php
@@ -5,3 +5,13 @@ $arr = array(1,1,2,3,3);
 
 for($i = 0; $i < count($arr); $i++) {}
 
+$arr = [1, 2, 2, 3, 4, 4, 5];
+$result = [];
+
+foreach ($arr as $value) {
+    if (!isset($result[$value])) {
+        $result[$value] = $value;
+    }
+}
+
+print_r($result);
