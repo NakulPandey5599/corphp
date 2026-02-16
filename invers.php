d96fce893dba7ad63e003d478c4d17527658ef03
NakulPandey <141328991+NakulPandey5599@users.noreply.github.com>
2026-02-16 23:50:29 +0530

    dublicate remove

--- invers.php
+++ invers.php
@@ -8,3 +8,17 @@ for ($i = strlen($str) - 1; $i >= 0; $i--) {
 
 echo $rev; // weivretni
 ?>
+
+
+<?php
+$arr = [1, 2, 2, 3, 4, 4, 5];
+$result = [];
+
+foreach ($arr as $value) {
+    if (!in_array($value, $result)) {
+        $result[] = $value;
+    }
+}
+
+print_r($result);
+?>
